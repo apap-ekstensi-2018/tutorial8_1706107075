3cb37496b7cb96dbfa91fc9966e74cb4e134676f
Winda Dumaria S <winda.dumaria@ruma.co.id>
2018-04-28 09:54:51 +0700

    login function

---
+++ APAPTutorial08/src/main/java/com/example/controller/PageController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+	@RequestMapping("/")
+    public String index (Model model)
+    {
+    		model.addAttribute("title", "Index");
+        return "index";
+    }
+	
+	@RequestMapping("/login")
+    public String login ()
+    {
+        return "login";
+    }
+	
+}
